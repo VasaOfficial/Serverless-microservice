f31bd1182fe5d66eb35135f6b47d560022239c88
Chili <vasapofficial@gmail.com>
2024-06-11 16:31:35 +0200

    feat: initialize the product-service with aws ask

---
+++ product-service/lib/product-service-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { ServiceStack } from './service-stack';
+import { ApiGatewayStack } from './api-gateway-stack';
+
+export class ProductServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const { productService } = new ServiceStack(this, 'ProductService', {});
+
+    new ApiGatewayStack(this, 'ProductApiGateway', {
+      productService,
+    });
+  }
+}
